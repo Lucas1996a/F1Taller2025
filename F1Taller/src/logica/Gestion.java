f78df7e5593e62f7614be22d809061c15af7c3cc
Lucas <teze90@gmail.com>
2025-11-06 16:01:03 -0300

    creacion de persistencia

---
+++ F1Taller/src/logica/Gestion.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Gestion {
+    
+    private ArrayList<Auto> listaAutos;
+    private ArrayList<Escuderia> listaEscuderias;
+    private ArrayList<Circuito> listaCircuitos;
+    private ArrayList<Piloto> listaPilotos;
+    private ArrayList<Mecanico> listaMecanicos;
+    private ArrayList<Pais> listaPais;
+    
+    /*
+    public Gestion(ArrayList<Auto> listaAutos, ArrayList<Escuderia> listaEscuderias, ArrayList<Circuito> listaCircuitos, ArrayList<Piloto> listaPilotos, ArrayList<Mecanicos> listaMecanicos, ArrayList<Pais> listaPais){
+        this.listaAutos = listaAutos;
+        this.listaEscuderias = listaEscuderias;
+        this.listaCircuitos = listaCircuitos;
+        this.listaPilotos = listaPilotos;
+        this.listaMecanicos = listaMecanicos;
+        this.listaPais = listaPais;
+    }
+ */
+    
+    public Gestion(){
+        this.listaAutos = new ArrayList<>();
+        this.listaEscuderias = new ArrayList<>();
+        this.listaCircuitos = new ArrayList<>();
+        this.listaPilotos = new ArrayList<>();
+        this.listaMecanicos = new ArrayList<>();
+        this.listaPais = new ArrayList<>();
+    }
+    
+    
+    public void agregarAutos(){
+        
+    }
+            
+    
+   
+      
+    
+    
+}
